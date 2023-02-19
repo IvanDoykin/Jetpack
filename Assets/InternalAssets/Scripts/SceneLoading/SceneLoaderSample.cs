64f22f251c53eb4588c5bcfa9e9acb05fa46fc2d
Ivan <52855695+IvanDoykin@users.noreply.github.com>
2023-02-19 23:24:18 +0300

    Remove old architecture, add scene loader

---
+++ Assets/InternalAssets/Scripts/SceneLoading/SceneLoaderSample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using SceneLoading;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderSample : MonoBehaviour
+{
+    [SerializeField] private SceneReference sceneToLoad;
+
+    private void Start()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
